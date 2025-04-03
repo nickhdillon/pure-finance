e3dc70b574f6f0f1a370ab44fb9d464a5c7fe774
Nick Dillon <nickhds@gmail.com>
2025-04-02 22:11:23 -0500

    Update logo

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -3,6 +3,6 @@
     <img src="{{ asset('icon.svg') }}" class="w-10 mb-0.5" />
 </div>
 
-<div class="ml-1 grid flex-1 text-left text-sm">
+<div class="grid flex-1 text-left text-sm">
     <span class="mb-0.5 truncate leading-none font-semibold">Pure Finance</span>
 </div>
