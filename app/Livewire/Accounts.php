4c3ebc52d8f71a0385e0c4f6516247c06a589dd2
Nick Dillon <nickhds@gmail.com>
2025-09-13 13:29:39 -0500

    fix: account query for groups

--- app/Livewire/Accounts.php
+++ app/Livewire/Accounts.php
@@ -18,43 +18,26 @@ class Accounts extends Component
 {
     public function render(): View
     {
-        $accounts = auth()->user()
+        $accounts = auth()
+            ->user()
             ->accounts()
             ->withCount('transactions')
             ->withSum([
-                // Banking accounts
-                'transactions as banking_cleared_deposits' => function (Builder $query): void {
-                    $query->whereIn('type', [TransactionType::CREDIT, TransactionType::DEPOSIT])
-                        ->whereIn('accounts.type', [AccountType::CHECKING, AccountType::SAVINGS])
-                        ->where('status', true);
+                // All transactions (available balance)
+                'transactions as all_deposits' => function (Builder $query): void {
+                    $query->whereIn('type', [TransactionType::CREDIT, TransactionType::DEPOSIT]);
                 },
-                'transactions as banking_cleared_debits' => function (Builder $query): void {
-                    $query->whereIn('type', [TransactionType::DEBIT, TransactionType::TRANSFER, TransactionType::WITHDRAWAL])
-                        ->whereIn('accounts.type', [AccountType::CHECKING, AccountType::SAVINGS])
-                        ->where('status', true);
+                'transactions as all_debits' => function (Builder $query): void {
+                    $query->whereIn('type', [TransactionType::DEBIT, TransactionType::TRANSFER, TransactionType::WITHDRAWAL]);
                 },
 
-                // Debt accounts
-                'transactions as debt_cleared_deposits' => function (Builder $query): void {
+                // Cleared only
+                'transactions as cleared_deposits' => function (Builder $query): void {
                     $query->whereIn('type', [TransactionType::CREDIT, TransactionType::DEPOSIT])
-                        ->whereIn('accounts.type', [AccountType::CREDIT_CARD, AccountType::LOAN])
                         ->where('status', true);
                 },
-                'transactions as debt_cleared_debits' => function (Builder $query): void {
+                'transactions as cleared_debits' => function (Builder $query): void {
                     $query->whereIn('type', [TransactionType::DEBIT, TransactionType::TRANSFER, TransactionType::WITHDRAWAL])
-                        ->whereIn('accounts.type', [AccountType::CREDIT_CARD, AccountType::LOAN])
-                        ->where('status', true);
-                },
-
-                // Investment accounts
-                'transactions as investment_cleared_deposits' => function (Builder $query): void {
-                    $query->whereIn('type', [TransactionType::CREDIT, TransactionType::DEPOSIT])
-                        ->where('accounts.type', AccountType::INVESTMENT)
-                        ->where('status', true);
-                },
-                'transactions as investment_cleared_debits' => function (Builder $query): void {
-                    $query->whereIn('type', [TransactionType::DEBIT, TransactionType::TRANSFER, TransactionType::WITHDRAWAL])
-                        ->where('accounts.type', AccountType::INVESTMENT)
                         ->where('status', true);
                 },
             ], 'amount')
@@ -62,10 +45,18 @@ class Accounts extends Component
             ->get()
             ->map(function (Account $account): Account {
                 if (in_array($account->type, [AccountType::LOAN, AccountType::CREDIT_CARD])) {
+                    $account->available_balance = $account->initial_balance
+                        - ($account->all_debits ?? 0)
+                        + ($account->all_deposits ?? 0);
+
                     $account->cleared_balance = $account->initial_balance
-                        - ($account->debt_cleared_debits ?? 0)
-                        + ($account->debt_cleared_deposits ?? 0);
+                        - ($account->cleared_debits ?? 0)
+                        + ($account->cleared_deposits ?? 0);
                 } else {
+                    $account->available_balance = $account->initial_balance
+                        + ($account->all_deposits ?? 0)
+                        - ($account->all_debits ?? 0);
+
                     $account->cleared_balance = $account->initial_balance
                         + ($account->cleared_deposits ?? 0)
                         - ($account->cleared_debits ?? 0);
@@ -82,15 +73,15 @@ class Accounts extends Component
 
         $grouped_accounts = collect($group_definitions)
             ->mapWithKeys(fn(array $types, string $group_name): array => [
-                $group_name => $accounts->whereIn('type', $types)
+                $group_name => $accounts->whereIn('type', $types),
             ])
             ->filter(fn(Collection $group_accounts): bool => $group_accounts->isNotEmpty())
             ->mapWithKeys(fn(Collection $group_accounts, string $group_name): array => [
                 $group_name => [
                     'accounts' => $group_accounts,
-                    'available_total' => $group_accounts->sum('balance'),
+                    'available_total' => $group_accounts->sum('available_balance'),
                     'cleared_total' => $group_accounts->sum('cleared_balance'),
-                ]
+                ],
             ]);
 
         return view('livewire.accounts', [
